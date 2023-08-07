4db76a94a79f8e358db48224b9f35b9748370518
edd <edd@vlending.co.kr>
2023-08-07 22:57:41 +0900

    Jetpack Compose 상태

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/WellnessTasksList.kt
@@ -0,0 +1,33 @@
+package com.codelabs.basicstatecodelab.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit,
+    list: List<WellnessTask> = remember { emptyList() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) }
+            )
+        }
+    }
+
+}
